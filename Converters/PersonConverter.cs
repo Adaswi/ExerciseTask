25cc7a8033ef536a5f197366e793c1276c316536
Adaswi <adamino2002@gmail.com>
2024-05-09 16:28:55 +0200

    Added person DTO with converter

---
+++ Converters/PersonConverter.cs
@@ -0,0 +1,18 @@
+﻿using IntegraTestTask.DTOs;
+using IntegraTestTask.Entities;
+
+namespace IntegraTestTask.Converters
+{
+    public static class PersonConverter
+    {
+        public static Person Convert(PersonDTO personDTO)
+        {
+            var person = new Person();
+            person.FirstName = personDTO.FirstName;
+            person.LastName = personDTO.LastName;
+            person.Birthdate = personDTO.Birthdate;
+            person.Address = personDTO.Address;
+            return person;
+        }
+    }
+}
---
+++ DTOs/PersonDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegraTestTask.DTOs
+{
+    public class PersonDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Address { get; set; }
+    }
+}
