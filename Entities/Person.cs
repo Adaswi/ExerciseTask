e3813af715fb0b6151e1c0a5dc5806789d3cabd0
Adaswi <adamino2002@gmail.com>
2024-05-09 12:33:19 +0200

    Add project files.

---
+++ Entities/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace IntegraTestTask.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Adress { get; set; }
+    }
+}
---
+++ TestDatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using IntegraTestTask.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegraTestTask
+{
+    public class TestDatabaseContext : DbContext
+    {
+        public TestDatabaseContext(DbContextOptions<TestDatabaseContext> options) : base(options) {  }
+        public DbSet<Person> People => Set<Person>();
+    }
+}
