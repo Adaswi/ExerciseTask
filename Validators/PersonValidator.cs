bf9cacb02c480a7cd9efc80b34142ee0f983a974
Adaswi <adamino2002@gmail.com>
2024-05-09 16:29:27 +0200

    Added validation

---
+++ Validators/PersonValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using IntegraTestTask.Entities;
+
+namespace IntegraTestTask.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().MaximumLength(255);
+            RuleFor(x => x.LastName).NotEmpty().MaximumLength(255);
+            RuleFor(x => x.Birthdate).NotEmpty().Must(birthdate => IsBirthdateValid(birthdate)).WithMessage("Data urodzenia musi być poprawna.");
+            RuleFor(x => x.Address).NotEmpty().EmailAddress();
+        }
+
+        public static bool IsBirthdateValid(DateTime birthdate)
+        {
+            return (birthdate <= DateTime.Now && birthdate > DateTime.Now.AddYears(-150));
+        }
+    }
+}
